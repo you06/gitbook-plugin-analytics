66e8b8219e27270ac15fdc26cd4d42cb10f8c2ec
you06 <you1474600@gmail.com>
2020-03-17 00:51:03 +0800

    ga for SPA
    Signed-off-by: you06 <you1474600@gmail.com>

--- vendor/google/code.js
+++ vendor/google/code.js
@@ -1,8 +1,8 @@
 if (GA_TOKEN) {
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');		
 
 	ga('create', GA_TOKEN, 'auto', 'tracker');
 	ga('tracker.send', 'pageview');
@@ -18,6 +18,7 @@ if (GA_TOKEN) {
 		}
 
 		if (startState.path !== nowState.path) {
+			ga('tracker.set', 'location', nowState.path);
 			ga('tracker.set', 'page', nowState.path);
 			ga('tracker.send', 'pageview');
 			startState = nowState;
